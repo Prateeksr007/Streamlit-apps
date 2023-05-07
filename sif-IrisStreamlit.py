26f05607011e228beff56ca64b9275b605343ac4
Prateeksr007 <98253501+Prateeksr007@users.noreply.github.com>
2023-05-07 21:24:31 +0530

    Update sif-IrisStreamlit.py

--- sif-IrisStreamlit.py
+++ sif-IrisStreamlit.py
@@ -7,7 +7,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from sklearn import datasets
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.svm import SVC
